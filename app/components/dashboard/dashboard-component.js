6c6c6af871eee219ba7a7058d8d27662c1e13421
my-spock <rplowman15@gmail.com>
2016-05-16 11:13:14 -0600

    becca push

--- app/components/dashboard/dashboard-component.js
+++ app/components/dashboard/dashboard-component.js
@@ -1,6 +1,6 @@
 app.controller('DashboardController', function($rootScope, $scope){
 	var dc = this;
-	
+
 	dc.editMember = function(member) {
 		dc.setUsername(member.username);
 		dc.setFirstName(member.firstName);
@@ -12,8 +12,6 @@ app.controller('DashboardController', function($rootScope, $scope){
 		dc.setGitHub(member.github);
 		dc.setTwitterHandle(member.twitterHandle);
 		dc.setFavorites(member.favoriteTags);
-		
-		dc.extras(member);
 		
 		console.log($rootScope.member);
 		
@@ -86,39 +84,18 @@ app.controller('DashboardController', function($rootScope, $scope){
 		}
 	}
 	
-	dc.extras = function(member) {
-		member.accountCreated = 
-	}
+	dc.questions = function findQuestions(){
+		
+	};
+	dc.answers = function findAnswers(){
+		
+	};
+	dc.comments = function findComments() {
+		
+	};
 
-	/**
-	 * To edit the member object you must go through $rootScope.member
-	 * $rootScope.member is a $firebaseObject from AngularFire 
-	 * To see the methods at your disposal look here
-	 * https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject
-	 * 
-	 * Don't forget to call $rootScope.member.$save() after making changes to the $rootScope.member object
-     * 
-     * A feature request would be to add an edit profile button to the view that can show and hide the edit form in your html
-	 * 
-	 * */
-	 
-	 	// 	username: string,
-		// firstName: string,
-		// lastName: string,
-		// imgUrl:  string,
-		// country: string,
-		// bio: string,
-		// websiteUrl: string,
-		// github: string,
-		// twitterHandle: string,
-		// favoriteTags: [tag], // <--- BONUS: use commas in the input field to add multiple tags. Or Look at multiselect inputs
-		// **accountCreated: date,
-		// **upVotes: number,
-		// **downVotes: number,
-		// **reputation: number,
-		// **questions: [questionId]
-		// **answers: [answerId],
-		// **comments: [commentId]
-		//Add anything else you think would be a cool feature to track
+	
+
+    //   A feature request would be to add an edit profile button to the view that can show and hide the edit form in your html
 	
 });
